70cc1951b7315f6bc24d2b5fcb70e3d3bca5295a
Matheus Set Inacio <c.inaciomatheus@gmail.com>
2024-06-01 17:55:51 -0300

    step 0 working

---
+++ impls/clojure.2/build.clj
@@ -0,0 +1,25 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'heyset/step0_repl)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "dist/classes")
+(def uber-file (format "dist/%s-standalone.jar" (name lib)))
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "dist"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile '[mal.step0-repl]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main 'mal.step0-repl}))
