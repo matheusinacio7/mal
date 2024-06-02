725e200a16b93e264b244545e677b9e6dd12d4c1
Matheus Set Inacio <c.inaciomatheus@gmail.com>
2024-06-02 16:37:14 -0300

    fills in read-atom, read-list and read-form

--- impls/clojure.2/src/mal/reader.clj
+++ impls/clojure.2/src/mal/reader.clj
@@ -1,5 +1,4 @@
-(ns mal.reader 
-  (:require [clojure.string :as str]))
+(ns mal.reader)
 
 (defn tokenize [str]
   (let [regex-expr #"[\s,]*(~@|[\[\]{}()'`~^@]|\"(?:\\.|[^\\\"])*\"?|;.*|[^\s\[\]{}('\"`,;)]*)"
@@ -8,21 +7,49 @@
          (map (fn [[_ captured]] captured))
          (filter #(not= "" %)))))
 
-(defn read-atom [atom]
-  atom)
+(defn string-integer? [str]
+  (try
+    (Integer/parseInt str)
+    true
+    (catch NumberFormatException _
+      false)))
 
-(defn read-list [list]
-  list)
+(defn read-atom [tokens]
+  (let [token (first tokens)
+        remaining (rest tokens)
+        atom (cond
+               (string-integer? token) {:value (Integer/parseInt token), :type :number}
+               (= "" token) (:type :eof)
+               :else {:name token :type :symbol})]
+    [atom remaining]))
 
-(defn read-form [form]
-  form)
+(declare read-form)
+
+(defn read-list [tokens]
+  (loop [current-list {:type :list :children []}
+         remaining (rest tokens)]
+    (let [next-token (first remaining)]
+      (cond
+        (= next-token ")") [current-list (rest remaining)]
+        (not-empty remaining) (let [[child remaining] (read-form remaining)]
+                                (recur (update current-list :children #(conj % child))
+                                       remaining))
+        :else (throw (ex-info "Unmatched (" {:type "Interpretation error"}))))))
+
+(defn read-form [tokens]
+  (case (first tokens)
+    "(" (read-list tokens)
+    (read-atom tokens)))
 
 (defn read-forms [tokens]
-  tokens)
+  (loop [forms []
+         remaining tokens]
+    (if (empty? remaining)
+      forms
+      (let [[new-form new-remaining] (read-form remaining)]
+        (recur (conj forms new-form) new-remaining)))))
 
 (defn read-str [str]
   (-> str
       tokenize
       read-forms))
-
-(tokenize "(  + 2   (*  3  4)  )  ")
