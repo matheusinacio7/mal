6dfa5d94184aac0b3d5bb8e5991efa523bc7a433
Matheus Set Inacio <c.inaciomatheus@gmail.com>
2024-06-06 17:08:37 -0300

    adds 'list?' empty? and count

--- impls/clojure.2/src/mal/core.clj
+++ impls/clojure.2/src/mal/core.clj
@@ -1,37 +1,57 @@
 (ns mal.core 
   (:require [mal.printer :as printer]))
 
-(def mal-ns {"+" {:type :clojure-function
-                  :fn (fn [& args]
-                        {:type :number
-                         :value (apply + (doall (map :value args)))})}
+(def mal-ns {"+"      {:type :clojure-function
+                       :fn   (fn [& args]
+                               {:type  :number
+                                :value (apply + (doall (map :value args)))})}
 
-             "-" {:type :clojure-function
-                  :fn (fn [& args]
-                        {:type :number
-                         :value (apply - (doall (map :value args)))})}
+             "-"      {:type :clojure-function
+                       :fn   (fn [& args]
+                               {:type  :number
+                                :value (apply - (doall (map :value args)))})}
 
-             "*" {:type :clojure-function
-                  :fn (fn [& args]
-                        {:type :number
-                         :value (apply * (doall (map :value args)))})}
+             "*"      {:type :clojure-function
+                       :fn   (fn [& args]
+                               {:type  :number
+                                :value (apply * (doall (map :value args)))})}
 
-             "/" {:type :clojure-function
-                  :fn (fn [& args]
-                        {:type :number
-                         :value (apply / (doall (map :value args)))})} 
+             "/"      {:type :clojure-function
+                       :fn   (fn [& args]
+                               {:type  :number
+                                :value (apply / (doall (map :value args)))})} 
              
-             "prn" {:type :clojure-function
-                    :fn (fn [& x]
-                          (println (printer/print-ast x))
-                          {:type :nil :value nil})}
+             "prn"    {:type :clojure-function
+                       :fn   (fn [& x]
+                               (println (printer/print-ast x))
+                               {:type  :nil
+                                :value nil})}
              
-             "list" {:type :clojure-function
-                     :fn (fn [& args]
-                           {:type :list
-                            :children (into [] args)})}
+             "list"   {:type :clojure-function
+                       :fn   (fn [& args]
+                               {:type     :list
+                                :children (into [] args)})}
 
-             "not" "(fn* (cond)
+             "list?"  {:type :clojure-function
+                       :fn   (fn [x]
+                               (let [y (if (= (:type x) :list)
+                                         true
+                                         false)]
+                                 {:type  :boolean
+                                  :value y}))}
+             
+             "empty?" {:type :clojure-function
+                       :fn   (fn [x]
+                               (let [y (empty? (:children x))]
+                                 {:type  :boolean
+                                  :value y}))}
+             
+             "count"  {:type :clojure-function
+                       :fn   (fn [x]
+                               {:type  :number
+                                :value (count (:children x))})}
+
+             "not"    "(fn* (cond)
                          (if cond false true))"})
 
 (into [] '(2 3 4))
